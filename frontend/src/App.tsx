afd5bb00b7980327accaa76c5680dc3434f4e8bd
Emilio Senguana <sengua2017@gmail.com>
2023-12-25 22:49:13 -0500

    Revert "add react-bootstrap"
    This reverts commit 207a154e28552a808de8d95be9d48ba6d76a9cf2.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,27 @@
- 
+
+import './App.css'
 import { sampleProducts } from './mocks/data'
-import {Navbar, Container, Nav, Row, Col} from 'react-bootstrap'
+
 function App() {
 
   return (
-    <div className='d-flex flex-column h-full'>
-      <header>
-        <Navbar bg='dark' variant='dark' expand="lg">
-          <Container>
-            <Navbar.Brand>TS Amazon</Navbar.Brand>
-          </Container>
-          <Nav>
-            <a href="/cart" className="nav-link">Cart</a>
-            <a href="/signin" className="nav-link">Sign In</a>
-          </Nav>
-        </Navbar>
-      </header>
+    <>
+      <header>TS Amazon</header>
       <main>
-       <Container className='mt-3'>
-          <Row>
-            {
-              sampleProducts.map((product, index) => (
-                <Col key={index} xs="6" md="4" lg={3}>
-                  <img src={product.image} alt="any" className='product-image' />
-                  <h2>{product.name}</h2>
-                  <p>${product.price}</p>
-                </Col>
-              ))
-            }
-          </Row>
-       </Container>
+        <ul>
+          {
+            sampleProducts.map((product, index) => (
+              <li key={index}>
+                <img src={product.image} alt="any" className='product-image' />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </li>
+            ))
+          }
+        </ul>
       </main>
-      <footer>
-        <div className="text-center">
-          All right reserved
-        </div>
-      </footer>
-    </div>
+      <footer>All right reserved</footer>
+    </>
   )
 }
 
