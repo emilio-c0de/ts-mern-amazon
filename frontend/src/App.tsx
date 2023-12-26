9d9e4f2bcf71aaeabeef30a717b85623b098e10f
Emilio Senguana <sengua2017@gmail.com>
2023-12-25 22:20:17 -0500

    add list products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { sampleProducts } from './mocks/data'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-       <header>TS Amazon</header>
-       <main></main>
-       <footer>All right reserved</footer>
+      <header>TS Amazon</header>
+      <main>
+        <ul>
+          {
+            sampleProducts.map((product, index) => (
+              <li key={index}>
+                <img src={product.image} alt="any" className='product-image' />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </li>
+            ))
+          }
+        </ul>
+      </main>
+      <footer>All right reserved</footer>
     </>
   )
 }
---
+++ frontend/src/mocks/data.ts
@@ -0,0 +1,124 @@
+import { Product } from "@/types/product.type";
+
+export const sampleProducts: Array<Product> = [
+    {
+        "name": "Producto 1",
+        "slug": "producto-1",
+        "category": "Categoría 1",
+        "image": "./images/p1.jpg",
+        "price": 29.99,
+        "countInStock": 50,
+        "brand": "Marca A",
+        "rating": 4.2,
+        "numRecives": 10,
+        "description": "Descripción del Producto 1"
+    },
+    {
+        "name": "Producto 2",
+        "slug": "producto-2",
+        "category": "Categoría 2",
+        "image": "./images/p2.jpg",
+        "price": 39.99,
+        "countInStock": 30,
+        "brand": "Marca B",
+        "rating": 3.8,
+        "numRecives": 8,
+        "description": "Descripción del Producto 2"
+    },
+    {
+        "name": "Producto 3",
+        "slug": "producto-3",
+        "category": "Categoría 1",
+        "image": "./images/p3.jpg",
+        "price": 49.99,
+        "countInStock": 20,
+        "brand": "Marca C",
+        "rating": 4.5,
+        "numRecives": 12,
+        "description": "Descripción del Producto 3"
+    },
+    {
+        "name": "Producto 4",
+        "slug": "producto-4",
+        "category": "Categoría 3",
+        "image": "./images/p4.jpg",
+        "price": 19.99,
+        "countInStock": 40,
+        "brand": "Marca A",
+        "rating": 4.0,
+        "numRecives": 15,
+        "description": "Descripción del Producto 4"
+    },
+    {
+        "name": "Producto 5",
+        "slug": "producto-5",
+        "category": "Categoría 2",
+        "image": "imagen-5.jpg",
+        "price": 59.99,
+        "countInStock": 25,
+        "brand": "Marca B",
+        "rating": 4.8,
+        "numRecives": 20,
+        "description": "Descripción del Producto 5"
+    },
+    {
+        "name": "Producto 6",
+        "slug": "producto-6",
+        "category": "Categoría 3",
+        "image": "imagen-6.jpg",
+        "price": 69.99,
+        "countInStock": 15,
+        "brand": "Marca C",
+        "rating": 3.5,
+        "numRecives": 5,
+        "description": "Descripción del Producto 6"
+    },
+    {
+        "name": "Producto 7",
+        "slug": "producto-7",
+        "category": "Categoría 1",
+        "image": "imagen-7.jpg",
+        "price": 79.99,
+        "countInStock": 10,
+        "brand": "Marca A",
+        "rating": 4.7,
+        "numRecives": 18,
+        "description": "Descripción del Producto 7"
+    },
+    {
+        "name": "Producto 8",
+        "slug": "producto-8",
+        "category": "Categoría 2",
+        "image": "imagen-8.jpg",
+        "price": 89.99,
+        "countInStock": 5,
+        "brand": "Marca B",
+        "rating": 4.3,
+        "numRecives": 14,
+        "description": "Descripción del Producto 8"
+    },
+    {
+        "name": "Producto 9",
+        "slug": "producto-9",
+        "category": "Categoría 3",
+        "image": "imagen-9.jpg",
+        "price": 99.99,
+        "countInStock": 12,
+        "brand": "Marca C",
+        "rating": 3.9,
+        "numRecives": 7,
+        "description": "Descripción del Producto 9"
+    },
+    {
+        "name": "Producto 10",
+        "slug": "producto-10",
+        "category": "Categoría 1",
+        "image": "imagen-10.jpg",
+        "price": 109.99,
+        "countInStock": 8,
+        "brand": "Marca A",
+        "rating": 4.1,
+        "numRecives": 22,
+        "description": "Descripción del Producto 10"
+    }
+]
